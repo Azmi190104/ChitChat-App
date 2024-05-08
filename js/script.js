8ca52b9dee611db8cbb1c42d6a36a0279cc6d000
M. Azmi Al Fadillah <120736981+Azmi190104@users.noreply.github.com>
2024-05-08 19:02:10 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const statusChat = document.querySelector('.globe')
+console.log(statusChat)
+const layerGlobe = document.querySelector('.layer-globe')
+const layerNav = document.querySelector('.nav')
+
+
+function tekan(){
+    layerGlobe.classList.toggle('.layer-globe2')
+}
